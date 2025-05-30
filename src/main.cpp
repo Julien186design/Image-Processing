ac16578516075740de12b258f6870173bc7cd1f5
Julien186design <julien94.beaudet@gmail.com>
2025-05-30 12:12:10 +0200

    Update main.cpp
    The input image are now in folders.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "ImageProcessor.h"
 
 int main() {
-    const std::string inputFile = "Skeleton.jpg";	//image to process
-    processImageTransforms(inputFile);
+    const std::string inputFile = "Barbie holds itself.jpg";	//image to process
+    const std::string folderName = "Barbie";	//folder
+    processImageTransforms(inputFile, folderName);
     return 0;
 }
