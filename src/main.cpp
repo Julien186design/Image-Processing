fbf9318f071497b18f790d69e2174a300583eb71
Julien186design <julien94.beaudet@gmail.com>
2025-08-16 16:37:59 +0200

    threshold outside the for loop
        int threshold = 40;
        int lastThreshold = 220;
        int step = 20;

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "ImageProcessoring.h"
 
 int main() {
-    const std::string inputFile = "Oreo cake.jpg";	//image to process
-    const std::string folderName = "Cake";	//folder
+    const std::string inputFile = "Oreo cake.jpg";
+    const std::string folderName = "Cake";
     
+    int threshold = 40;
+    int lastThreshold = 220;
+    int step = 20;
+    		
     // Extract the base name without the extension
     size_t dotPos = inputFile.find_last_of('.');
     std::string baseName = inputFile.substr(0, dotPos);
@@ -12,9 +16,8 @@ int main() {
     std::string inputPath = "Input/" + folderName + "/" + inputFile;
     printf("%s\n", inputPath.c_str()); // Print the input path
     printf("%s\n", folderName.c_str()); // Print the folder name
-    
-    
-	processImageTransforms(inputPath, baseName);
-	
+        
+	processImageTransforms(inputPath, baseName, threshold, lastThreshold, step);
+
     return 0;
 }
