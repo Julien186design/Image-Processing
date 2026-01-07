0dfb021094ab3e8f935df123f65e46fb34086932
Julien186design <julien94.beaudet@gmail.com>
2026-01-07 13:28:30 +0100

    Refactor image processing parameters and update output

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,17 @@ int main() {
 	auto start = std::chrono::high_resolution_clock::now();
     const std::string inputFile = "Oreo Cake.jpg";
     const std::string folderName = "Food";
-
-	int first_threshold = 30;
-	int last_threshold = 240;
-	int step = 30;
+	
+	std::vector<int> thresholdsAndStep = {30, 240, 30}; // {first_Threshold, last_Threshold, step}
 	int fraction = 2;
-	std::vector<int> tolerance = {0, 100, 5};
+	std::vector<int> tolerance = {0, 20, 1};
 	std::vector<int> rectanglesToModify = {0, 5, 10, 15};
 	bool totalStepByStepT = false;
 	bool totalBlackAndWhiteT = false;
 	bool totalReversalT = false;
-	bool partialT = false;
+	bool partialT = true;
 	bool alternatingBlackAndWhite = false; // the function is still under construction
-	bool oneColor = true;
+	bool oneColor = false;
     		
     // Extract the base name without the extension
     size_t dotPos = inputFile.find_last_of('.');
@@ -31,14 +29,14 @@ int main() {
     std::string inputPath = "Input/" + folderName + "/" + inputFile;
     printf("%s\n", folderName.c_str()); // Print the folder name
         
-	processImageTransforms(inputPath, baseName, first_threshold,
-		last_threshold,	step, fraction, rectanglesToModify, tolerance, totalStepByStepT,
-		totalBlackAndWhiteT, totalReversalT, partialT,alternatingBlackAndWhite, oneColor);
+	processImageTransforms(inputPath, baseName, thresholdsAndStep, fraction, rectanglesToModify, tolerance,
+		totalStepByStepT, totalBlackAndWhiteT, totalReversalT,
+		partialT,alternatingBlackAndWhite, oneColor);
 
 	auto end = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
-	printf("Temps d'exécution: %ld s\n", duration.count());
+	printf("Execution time: %ld s\n", duration.count());
 
     return 0;
 }
