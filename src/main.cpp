dca515f64e5b467e2b3d3724c3ba86ab43a2049c
Julien186design <julien94.beaudet@gmail.com>
2026-01-21 18:27:43 +0100

    Refactor image processing parameters and logic

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,27 @@ int main() {
 	auto start = std::chrono::high_resolution_clock::now();
 	const std::string inputFile = "Oreo Cake.jpg";
 	const std::string folderName = "Food";
-
-	const std::vector<int> thresholdsAndStep = {180, 250, 10}; // {first Threshold, last Threshold, step}
-	// const std::vector<int> colorNuances = {0, 20, 40, 60, 80, 90, 110};
-	const std::vector<int> colorNuances = {0, 90};
-	constexpr int fraction = 4;
-	// const std::vector<int> rectanglesToModify = {0, 5, 10, 15};
-	const std::vector<int> rectanglesToModify = genererRectanglesInDiagonale(fraction);
-	const std::vector<int> toleranceOneColor = {0, 1, 1};
+	
+	const std::vector<int> thresholdsAndStep = {60, 250, 60}; // {first threshold, last threshold, step}
+	const std::vector<int> colorNuances = {0, 120, 40}; // {first color, last color, step}
+	constexpr int fraction = 2;
+	std::vector<int> rectanglesToModify = range_to_vector({8, 15});
+	const std::vector<int> toleranceOneColor = {0, 20, 4};
 	constexpr bool severalColors = false;
-	constexpr bool totalBlackAndWhiteT = false;
-	constexpr bool totalReversalT = false;
-	constexpr bool partialT = true;
+	constexpr bool oneColor = true;
+	constexpr bool totalBlackAndWhite = false;
+	constexpr bool totalReversal = false;
+	constexpr bool partial = false;
+	constexpr bool partialInDiagonal = false;
 	constexpr bool alternatingBlackAndWhite = false; // the function is still under construction
-	constexpr bool oneColor = false;
 
 
 	processImageTransforms(inputFile, folderName, thresholdsAndStep, colorNuances, fraction,
-		rectanglesToModify,	toleranceOneColor, severalColors, totalBlackAndWhiteT, totalReversalT,
-		partialT,alternatingBlackAndWhite, oneColor);
+		rectanglesToModify,	toleranceOneColor, severalColors, totalBlackAndWhite, totalReversal,
+		partial, partialInDiagonal, alternatingBlackAndWhite, oneColor);
 
-	for (const auto& valeur : rectanglesToModify) {
-		std::cout << valeur << " ";
-	}
-	std::cout << std::endl;
-
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-
+	const auto end = std::chrono::high_resolution_clock::now();
+	const auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
 	printf("Execution time: %ld s\n", duration.count());
 
