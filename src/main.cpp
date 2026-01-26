5a1817fc08f85b640164ac46d8266623991b13d4
Julien186design <julien94.beaudet@gmail.com>
2026-01-26 10:39:11 +0100

    Refactor image processing parameters and paths
    Updated input file path and modified parameters for image processing functions.

--- src/main.cpp
+++ src/main.cpp
@@ -10,26 +10,34 @@ int main() {
 		std::cout << "Thread " << omp_get_thread_num() << " sur " << omp_get_num_threads() << std::endl;
 	}
 	auto start = std::chrono::high_resolution_clock::now();
-	const std::string inputFile = "Oreo Cake.jpg";
-	const std::string folderName = "Food";
+	const std::string inputFile = "Food/Oreo Cake.jpg";
 	
-	const std::vector<int> thresholdsAndStep = {60, 250, 60}; // {first threshold, last threshold, step}
-	const std::vector<int> colorNuances = {0, 120, 40}; // {first color, last color, step}
+	const std::vector<int> thresholdsAndStep = {60, 180, 30}; // {first threshold, last threshold, step}
+	const std::vector<float> proportions = {0, 1, 0.1};
+	const std::vector<int> colorNuances = {0, 120, 10}; // {first color, last color, step}
+	constexpr int fps = 25;
 	constexpr int fraction = 2;
 	std::vector<int> rectanglesToModify = range_to_vector({8, 15});
-	const std::vector<int> toleranceOneColor = {0, 20, 4};
-	constexpr bool severalColors = false;
-	constexpr bool oneColor = true;
+	const std::vector toleranceOneColor = {0, 20, 1};
+	constexpr bool severalColorsByThreshold = false;
+	constexpr bool severalColorsByProportion = false;
+	constexpr bool oneColor = false;
 	constexpr bool totalBlackAndWhite = false;
 	constexpr bool totalReversal = false;
 	constexpr bool partial = false;
 	constexpr bool partialInDiagonal = false;
 	constexpr bool alternatingBlackAndWhite = false; // the function is still under construction
 
+	const auto [baseName, inputPath] = extractImageInfo(inputFile);
 
-	processImageTransforms(inputFile, folderName, thresholdsAndStep, colorNuances, fraction,
-		rectanglesToModify,	toleranceOneColor, severalColors, totalBlackAndWhite, totalReversal,
-		partial, partialInDiagonal, alternatingBlackAndWhite, oneColor);
+	processImageTransforms(baseName, inputPath, thresholdsAndStep, proportions, colorNuances, fraction,
+		rectanglesToModify,	toleranceOneColor, severalColorsByThreshold, severalColorsByProportion,
+		totalBlackAndWhite, totalReversal, partial, partialInDiagonal, alternatingBlackAndWhite, oneColor);
+
+	
+	std::cout << inputPath << std::endl;
+
+	several_colors_transformations_streaming(baseName, inputPath,  fps, proportions, colorNuances);
 
 	const auto end = std::chrono::high_resolution_clock::now();
 	const auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
