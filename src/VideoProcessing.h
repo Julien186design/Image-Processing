e6f2330588d9b152dd173ba6b23b30e7361cd78a
Julien186design <julien94.beaudet@gmail.com>
2026-01-29 17:29:56 +0100

    Remove unused function declarations from VideoProcessing.h

--- src/VideoProcessing.h
+++ src/VideoProcessing.h
@@ -4,22 +4,6 @@
 #include <vector>
 #include <string>
 
-
-void several_colors_transformations_streaming(
-    const std::string& baseName,
-    const std::string& inputPath,
-    int fps,
-    const std::vector<float> &proportions,
-    const std::vector<int>& colorNuances
-);
-
-void edge_detector_video(
-    const std::string& baseName,
-    const std::string& inputVideoPath,
-    const std::vector<int>& frames
-
-);
-
 void processVideoTransforms(
     const std::string& baseName,
     const std::string& inputPath,
