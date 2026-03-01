f62323915cace42b0216beced7b8496ae8e74fb0
Julien186design <julien94.beaudet@gmail.com>
2026-03-01 11:25:41 +0100

    Simplify main function by removing unused code
    Removed timing and notification features from main function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,6 @@
-#include "ImageProcessing.h"
 #include "ProcessingConfig.h"
-#include <chrono>
-#include <cstdlib>
 
 int main() {
-	const auto start = std::chrono::high_resolution_clock::now();
-	const std::string inputFile = "Food/Oreo Cake.jpg";
-    const auto [baseName, inputPath] = extractImageInfo(inputFile);
-
-    image_and_video_processing(baseName, inputPath);
-
-    const auto end = std::chrono::high_resolution_clock::now();
-    const auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-    printf("Execution time: %ld s\n", duration.count());
-
-    // Send a desktop notification when the program finishes
-    const std::string notificationMessage = baseName + "\nThe image and video processing has finished in " +
-                                            std::to_string(duration.count()) + " seconds.";
-    sendNotification("Program Execution Complete", notificationMessage);
-
+    image_and_video_processing("Food/Oreo Cake.jpg");
     return 0;
 }
