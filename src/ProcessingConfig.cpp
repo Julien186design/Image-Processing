013c33f4f3509ac6cb6ffb611d46a1edcd248c92
Julien186design <julien94.beaudet@gmail.com>
2026-03-01 11:26:04 +0100

    Refactor image_and_video_processing function
    Refactored image_and_video_processing to use a single inputFile parameter and added timing for processing duration.

--- src/ProcessingConfig.cpp
+++ src/ProcessingConfig.cpp
@@ -1,21 +1,29 @@
 #include "ProcessingConfig.h"
-
 #include "ImageProcessing.h"
 #include "VideoProcessing.h"
 
-void image_and_video_processing(
-    const std::string& baseName,
-    const std::string& inputPath) {
+#include <chrono>
+
+void image_and_video_processing(const std::string& inputFile) {
+
+    const auto start = std::chrono::high_resolution_clock::now();
+
+    const auto [baseName, inputPath] = extractImageInfo(inputFile);
 
     const parameters cfg;
 
-    processImageTransforms(baseName, inputPath, cfg.proportions, cfg.colorNuances, cfg.fraction,
-        cfg.rectangles, cfg.toleranceOneColor, cfg.weightOfRGB, cfg.severalColorsByProportion,
-        cfg.totalReversal, cfg.partial, cfg.partialInDiagonal, cfg.oneColor, cfg.average);
+    processImageTransforms(baseName, inputPath, cfg.proportions, cfg.colorNuances,
+        cfg.rectangles, cfg.toleranceOneColor, cfg.weightOfRGB);
 
-    processVideoTransforms(baseName, inputPath, cfg.fps, cfg.proportions, cfg.colorNuances,
-                           cfg.frames, cfg.toleranceOneColor, cfg.weightOfRGB, cfg.average);
+    processVideoTransforms(baseName, inputPath, cfg.proportions, cfg.colorNuances,
+                           cfg.frames, cfg.toleranceOneColor, cfg.weightOfRGB);
 
+    const auto end = std::chrono::high_resolution_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+
+    const std::string message = baseName + "\nThe image and video processing has finished in " +
+        std::to_string(duration.count()) + " seconds.";
+    sendNotification("Program Execution Complete", message);
 
 
 }
