970b390ebaf6fc8a121848223c7fc17f813693ca
Julien186design <julien94.beaudet@gmail.com>
2026-04-01 17:41:19 +0200

    Add reverse_transformations_by_proportion_streaming function

--- src/VideoCreation.h
+++ src/VideoCreation.h
@@ -3,6 +3,21 @@
 
 #include <string>
 
+void reverse_transformations_by_proportion_streaming(
+    const std::string& baseName,
+    const std::string& inputPath
+);
+
+void processVideoTransforms(
+    const std::string& baseName ,
+    const std::string& inputPath
+);
+
+#endif //IMAGE_PROCESSING_VIDEOCREATION_H#ifndef IMAGE_PROCESSING_VIDEOCREATION_H
+#define IMAGE_PROCESSING_VIDEOCREATION_H
+
+#include <string>
+
 void processVideoTransforms(
     const std::string& baseName ,
     const std::string& inputPath
