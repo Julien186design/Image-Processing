862d9212e1e4b9ffedd98b6b6214c183e8fc5a03
Julien186design <julien94.beaudet@gmail.com>
2026-04-13 10:15:08 +0200

    Clean up VideoCreation.h header guards
    Removed redundant header guards and function declaration.

--- src/VideoCreation.h
+++ src/VideoCreation.h
@@ -13,14 +13,4 @@ void processVideoTransforms(
     const std::string& inputPath
 );
 
-#endif //IMAGE_PROCESSING_VIDEOCREATION_H#ifndef IMAGE_PROCESSING_VIDEOCREATION_H
-#define IMAGE_PROCESSING_VIDEOCREATION_H
-
-#include <string>
-
-void processVideoTransforms(
-    const std::string& baseName ,
-    const std::string& inputPath
-);
-
 #endif //IMAGE_PROCESSING_VIDEOCREATION_H
